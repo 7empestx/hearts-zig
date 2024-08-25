a799909ccf894370dfed6bd254e31b2c92a3dbd7
7empestx <grant.starkman@gmail.com>
2024-08-25 00:01:09 -0600

    pass cards impl

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,22 @@
 const std = @import("std");
 const print = std.debug.print;
 const RndGen = std.rand.DefaultPrng;
-const spoon_menu = @import("spoon_menu.zig");
 
 const cards_in_deck: u8 = 52;
 const player_count: u8 = 4;
+const cards_to_pass: u8 = 3;
 const cards_per_player: u8 = cards_in_deck / player_count;
+const pass_cards_store: u8 = player_count * cards_to_pass;
 
-const Card = struct { suit: []const u8, rank: []const u8 };
+const Card = struct { suit: []const u8, rank: []const u8, passed: bool };
 const Deck = struct { cards: [cards_in_deck]Card };
 const Player = struct { player_cards: [cards_per_player]Card };
 const Game = struct { players: [player_count]Player };
 const Passing = enum { Right, Left, Across };
+const PassCardSelection = struct { firstCard: u64, secondCard: u64, thirdCard: u64 };
+const PassCardStore = struct { cards: [pass_cards_store]Card };
 
 pub fn main() !void {
-    //try spoon_menu.spoonInit();
-
     // Create a base deck of cards
     const deck = generateDeck();
 
@@ -24,25 +25,84 @@ pub fn main() !void {
 
     // Assign the first 13 cards to the first player and so on
     const players = dealCardsToPlayers(gameDeck);
-    printPlayer(players[0]);
+    printPlayer(players[0], "Player 1");
 
     // Create game
-    const game = Game{ .players = players };
-    _ = game;
+    var game = Game{ .players = players };
 
-    try readPlayerCardSelection();
+    const passCardSelection = try readPlayerCardSelection();
+
+    var passCardStore = PassCardStore{ .cards = undefined };
+
+    const direction = Passing.Right;
+    passCards(passCardSelection, &passCardStore, direction, &game);
 }
 
-fn readPlayerCardSelection() !void {
-    const firstCard = try userInput("first");
-    const secondCard = try userInput("second");
-    const thirdCard = try userInput("third");
-    passCards(firstCard, secondCard, thirdCard);
+fn passCards(passCardSelection: PassCardSelection, passCardStore: *PassCardStore, direction: Passing, game: *Game) void {
+    _ = direction;
+    // Player 1 Card Store
+    passCardStore.cards[0] = game.players[0].player_cards[passCardSelection.firstCard];
+    passCardStore.cards[1] = game.players[0].player_cards[passCardSelection.secondCard];
+    passCardStore.cards[2] = game.players[0].player_cards[passCardSelection.thirdCard];
+    game.players[0].player_cards[passCardSelection.firstCard].passed = true;
+    game.players[0].player_cards[passCardSelection.secondCard].passed = true;
+    game.players[0].player_cards[passCardSelection.thirdCard].passed = true;
+
+    // Player 2 Card Store
+    var card1 = getRandomPlayerCard();
+    var card2 = getRandomPlayerCard();
+    var card3 = getRandomPlayerCard();
+    passCardStore.cards[3] = game.players[1].player_cards[card1];
+    passCardStore.cards[4] = game.players[1].player_cards[card2];
+    passCardStore.cards[5] = game.players[1].player_cards[card3];
+    game.players[1].player_cards[card1].passed = true;
+    game.players[1].player_cards[card2].passed = true;
+    game.players[1].player_cards[card3].passed = true;
+
+    // Player 3 Card Store
+    card1 = getRandomPlayerCard();
+    card2 = getRandomPlayerCard();
+    card3 = getRandomPlayerCard();
+    passCardStore.cards[6] = game.players[2].player_cards[card1];
+    passCardStore.cards[7] = game.players[2].player_cards[card2];
+    passCardStore.cards[8] = game.players[2].player_cards[card3];
+    game.players[2].player_cards[card1].passed = true;
+    game.players[2].player_cards[card2].passed = true;
+    game.players[2].player_cards[card3].passed = true;
+
+    // Player 4 Card Store
+    card1 = getRandomPlayerCard();
+    card2 = getRandomPlayerCard();
+    card3 = getRandomPlayerCard();
+    passCardStore.cards[9] = game.players[3].player_cards[card1];
+    passCardStore.cards[10] = game.players[3].player_cards[card2];
+    passCardStore.cards[11] = game.players[3].player_cards[card3];
+    game.players[3].player_cards[card1].passed = true;
+    game.players[3].player_cards[card2].passed = true;
+    game.players[3].player_cards[card3].passed = true;
+
+    // Player 2 pick up cards from card store 0, 1, 2
+    var j: u8 = 0;
+    for (game.players[1].player_cards, 0..) |p, i| {
+        print("{s} of {s} {}\n", .{ p.rank, p.suit, p.passed });
+        if (p.passed) {
+            print("Player 2 picked up card from store\n", .{});
+            game.players[1].player_cards[i] = passCardStore.cards[j];
+            j += 1;
+        }
+    }
 }
 
-fn passCards(firstCard: u64, secondCard: u64, thirdCard: u64) void {
-    // Implement the logic for passing cards here
-    std.debug.print("Passing cards: {d}, {d}, {d}\n", .{ firstCard, secondCard, thirdCard });
+fn readPlayerCardSelection() !PassCardSelection {
+    return .{ .firstCard = try userInput("first"), .secondCard = try userInput("second"), .thirdCard = try userInput("third") };
+}
+
+fn getRandomPlayerCard() u8 {
+    const min = 0;
+    const max = cards_per_player - 1;
+
+    const rand = generateRandomNumber(min, max);
+    return rand;
 }
 
 fn userInput(cardString: []const u8) !u64 {
@@ -72,10 +132,10 @@ fn dealCardsToPlayers(gameDeck: Deck) [4]Player {
     return [4]Player{ player1, player2, player3, player4 };
 }
 
-fn printPlayer(player: Player) void {
-    print("Printing Player Cards\n", .{});
+fn printPlayer(player: Player, thePlayer: []const u8) void {
+    print("Printing Player Cards for {s}\n", .{thePlayer});
     for (player.player_cards, 0..) |c, idx| {
-        print("{}: {s} of {s}\n", .{ idx, c.rank, c.suit });
+        print("{}: {s} of {s} {}\n", .{ idx, c.rank, c.suit, c.passed });
     }
     print("\n", .{});
 }
@@ -90,8 +150,11 @@ fn printDeck(deck: Deck) void {
 
 fn shuffleDeck(deck: Deck) Deck {
     var s_deck = deck;
+    const min = 0;
+    const max = cards_in_deck - 1;
+
     for (s_deck.cards[0..], 0..) |_, idx| {
-        const r = generateRandomNumber();
+        const r = generateRandomNumber(min, max);
         const temp_card = s_deck.cards[idx];
         s_deck.cards[idx] = s_deck.cards[r];
         s_deck.cards[r] = temp_card;
@@ -99,9 +162,9 @@ fn shuffleDeck(deck: Deck) Deck {
     return s_deck;
 }
 
-fn generateRandomNumber() u8 {
+fn generateRandomNumber(min: u8, max: u8) u8 {
     const rand = std.crypto.random;
-    return rand.intRangeAtMost(u8, 0, 51);
+    return rand.intRangeAtMost(u8, min, max);
 }
 
 fn generateDeck() Deck {
@@ -112,7 +175,7 @@ fn generateDeck() Deck {
     var card_idx: usize = 0;
     for (suits) |s| {
         for (ranks) |r| {
-            deck.cards[card_idx] = Card{ .suit = s, .rank = r };
+            deck.cards[card_idx] = Card{ .suit = s, .rank = r, .passed = false };
             card_idx += 1;
         }
     }
@@ -124,12 +187,12 @@ test "Deal Cards to Players" {
     const gameDeck = shuffleDeck(deck);
     const players = dealCardsToPlayers(gameDeck);
     for (players) |p| {
-        printPlayer(p);
+        printPlayer(p, "Player");
     }
 }
 
 test "Random Number Generation" {
-    const rand = generateRandomNumber();
+    const rand = generateRandomNumber(0, 51);
     try std.testing.expect(rand >= 0);
     try std.testing.expect(rand <= 51);
 }
